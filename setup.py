0977edf76b3d231d0536c7f8f35fbd21a1b9b806
Hamd Umer <haammd.umer@gmail.com>
2025-12-11 23:48:49 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Hearable-chatbot",
+    version="0.1.0",
+    author="hamd",
+    author_email="haammd.umer@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
